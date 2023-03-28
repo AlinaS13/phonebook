aae30d0d7e2c049aefd981cad336534bcae6a7f6
AlinaS13 <soulofangel13@gmail.com>
2023-03-28 22:02:28 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Wrapper from 'components/Wrapper';
 import { PhoneBook } from 'pages/contacts/PhoneBook';
-import Login from 'pages/login/Login';
+import Login from './pages/login/Login';
 import Registration from 'pages/registration/Registration';
 import NotFound from 'pages/not-found/notFound';
 import Home from 'pages/home/Home';
