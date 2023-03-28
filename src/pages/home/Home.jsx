81e6d51e1f56ba330326d9eb8a8d8ec5ff34f7f6
AlinaS13 <soulofangel13@gmail.com>
2023-03-28 22:28:11 +0300

    add fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,31 @@
+import styled from 'styled-components';
 export default function Home() {
-  return <h1>Welcome!!!!</h1>;
+  return (
+    <Wrap>
+      <Title>Welcome!!!!</Title>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Text>
+    </Wrap>
+  );
 }
+export const Wrap = styled.div`
+  margin: 0, auto;
+  padding: 40px;
+`;
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: 100px;
+  font-size: 42px;
+`;
+
+export const Text = styled.p`
+  text-align: center;
+  font-size: 30px;
+`;
